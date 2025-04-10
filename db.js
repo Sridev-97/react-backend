87a2927497d87aae7d5d107ae5726b0b5e595c90
Sridev-97 <sridharmahadev18@gmail.com>
2025-04-11 00:32:37 +0530

    updated db,js

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
-const {MongoClient} = require('mongodb');
+require('dotenv').config(); // load env variables
+const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-async function connectToMongo(){
-  try{
+async function connectToMongo() {
+  try {
     await client.connect();
     console.log("✅ Connected to MongoDB Atlas");
 
     const db = client.db("rootsDB");
     return db;
-  }catch (err){
+  } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
     return null;
   }
